00f8df3c4171531e51671623880df789ca79f3c7
santiagosada <ssada96@gmail.com>
2017-06-13 11:53:51 -0500

    Added ImageTableViewCell and TextMentionTableViewCell

---
+++ SmashTagBeta/ImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageTableViewCell.swift
+//  SmashTagBeta
+//
+//  Created by Santiago Sada on 6/13/17.
+//  Copyright © 2017 MegaPizza. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SmashTagBeta/TextMentionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextMentionTableViewCell.swift
+//  SmashTagBeta
+//
+//  Created by Santiago Sada on 6/13/17.
+//  Copyright © 2017 MegaPizza. All rights reserved.
+//
+
+import UIKit
+
+class TextMentionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
