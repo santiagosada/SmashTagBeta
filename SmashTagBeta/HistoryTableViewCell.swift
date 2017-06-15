a4c0fd377a75835de34b6329f933d6fab855d984
santiagosada <ssada96@gmail.com>
2017-06-14 19:00:28 -0500

    Embedded the project in a TabView, and added HistoryTableViewController and HistoryTableViewCell

---
+++ SmashTagBeta/HistoryTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  HistoryTableViewCell.swift
+//  SmashTagBeta
+//
+//  Created by Santiago Sada on 6/14/17.
+//  Copyright © 2017 MegaPizza. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var HistoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
